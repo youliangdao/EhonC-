46a874d2ed498f5ce5ae4dd58196509d9581990a
youliangdao <youliangdaojing5@gmail.com>
2021-06-22 20:44:10 +0900

    sample5練習

--- chap01/sample5.cc
+++ chap01/sample5.cc
@@ -2,22 +2,33 @@
 
 class MikanBox{
   public:
+      MikanBox();
+      ~MikanBox();
       void Add(int addmikan); //みかんを入れる
       void Del(int delmikan); //みかんを取り出す
       void Empty(); // 箱を空にする
       int GetTotal(){
-        return total;
+        return total * 50;
       }
   private:
       int total;
 };
 
+MikanBox::MikanBox(){
+  total = 0;
+}
+
+MikanBox::~MikanBox(){
+  printf("みかん箱オブジェクトの任務完了\n");
+}
+
 void MikanBox::Add(int addmikan){
-  total += addmikan;
+  total += addmikan * 50;
+  if (total >= 5000) printf("合計が100個を超えました\n");
 }
 
 void MikanBox::Del(int delmikan){
-  total -= delmikan;
+  total -= delmikan * 50;
   if (total <= 0) Empty();
 }
 
@@ -28,9 +39,8 @@ void MikanBox::Empty(){
 int main()
 {
   MikanBox myMikanBox;
-
-  myMikanBox.Add(5);
-  myMikanBox.Del(2);
-  printf("箱の中のみかん：%d個\n", myMikanBox.GetTotal());
+  myMikanBox.Add(124);
+  myMikanBox.Del(10);
+  printf("箱の中のみかん：%d個\n", myMikanBox.GetTotal() / 50);
   return 0;
 }
